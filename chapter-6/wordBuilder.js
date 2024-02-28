76cf447d5a5666a2c16a39a173f60357a98607aa
Rukshona Rad <rukshona.rad@gmail.com>
2024-02-28 09:04:15 -0500

    Solve wordBuilder

---
+++ chapter-6/wordBuilder.js
@@ -0,0 +1,15 @@
+function wordBuilder(array) {
+    let collection = [];
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (i !== j) {
+                collection.push(array[i] + array[j]);
+            }
+        }
+    }
+
+    return collection;
+}
+
+console.log(wordBuilder(["apple", "banana", "orange"]));
