886dc6de5d2aa5c6585f67917cfde2e252e000f2
Rukshona Rad <rukshona.rad@gmail.com>
2024-02-29 09:02:30 -0500

    Solve nested array

--- chapter-6/wordBuilder.js
+++ chapter-6/wordBuilder.js
@@ -13,3 +13,18 @@ function wordBuilder(array) {
 }
 
 console.log(wordBuilder(["apple", "banana", "orange"]));
+
+function threeLoop(array) {
+    let collection = [];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            for (let k = 0; k < array.length; k++) {
+                if (i !== j && j !== k && i !== k) {
+                    collection.push(array[i] + array[j] + array[k]);
+                }
+            }
+        }
+    }
+    return collection;
+}
+console.log(threeLoop(["a", "b", "c", "d"]));
